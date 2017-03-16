584c3cf2c05e23ba6c0c38408216511dfe7fb9a5
Andranik Simonyan <asimonyan@eua.bvcorp.corp>
2017-03-16 12:56:01 +0200

    - Рефакоринг

--- src/routes/AppHome/containers/AppHomeContainer.js
+++ src/routes/AppHome/containers/AppHomeContainer.js
@@ -1,4 +1,22 @@
 import AppHome from '../components/HomeView';
+import { connect } from 'react-redux';
+import { onSetOpen, onSetDocked } from '../../App/modules/app';
 
-export default AppHome;
+const mapStateToProps = (state) => {
+    return {
+        docked: state.getIn(['app', 'sidebar', 'sidebarDocked'])
+    };
+};
 
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onSetOpen: (open) => {
+            dispatch(onSetOpen(open));
+        },
+        onSetDocked: (docked) => {
+            dispatch(onSetDocked(docked));
+        }
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(AppHome);
