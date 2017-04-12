98a090a1f5fe47829b5a2c97932a9669d8513f28
Andranik Simonyan <andranik.s.s@gmail.com>
2017-04-12 10:10:54 +0300

    lint --fix

--- src/containers/UserMenu/index.js
+++ src/containers/UserMenu/index.js
@@ -42,7 +42,7 @@ class UserMenu extends React.Component {
                 }
                     id='user-dropdown' eventKey='2'
                 >
-                    <LinkContainer to='/profile' className="media">
+                    <LinkContainer to='/profile' className='media'>
                         <MenuItem eventKey='2.1'>
                             <img className='media-object user-photo' src={currentUser.providerData[0].photoURL} />
                         </MenuItem>
