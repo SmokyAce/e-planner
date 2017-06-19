5cb03c90c74c785fdc014b0192845bdaec9b770f
Andranik Simonyan (STORM) <andranik.s.s@gmail.com>
2017-06-19 22:29:56 +0300

    - Change Footer style

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,11 +5,9 @@ import './Footer.scss';
 
 const Footer = () => (
     <footer>
-        <ul>
-            <hr className='line' />
-            <p>Copyright &copy; 2017 - Built By Andranik Simonyan</p>
-            <a href='mailto:andranik.s.s@gmail.com'>andranik.s.s@gmail.com</a>
-        </ul>
+        <hr className='line' />
+        <p>Copyright &copy; 2017 - Built By Andranik Simonyan</p>
+        <a href='mailto:andranik.s.s@gmail.com'>andranik.s.s@gmail.com</a>
     </footer>
 );
 
