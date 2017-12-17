4f4253f42572e474bc4044ffc455aa8159875e5f
Andranik Simonyan <andranik.s.s@gmail.com>
2017-12-17 15:29:17 +0200

    Change event home page messages

--- src/routes/EventHome/components/HomeView.jsx
+++ src/routes/EventHome/components/HomeView.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Map } from 'immutable';
 import { FormattedMessage } from 'react-intl';
 import messages from './messages';
 
-
-export const HomeView = () => {
+export const HomeView = props => {
     return (
         <div>
-            <h2><FormattedMessage {...messages.greeting} /></h2>
+            <h2>
+                {props.eventEntry.get('name')} <FormattedMessage {...messages.description} />
+            </h2>
         </div>
     );
 };
 
-HomeView.propTypes = {};
+HomeView.propTypes = {
+    eventEntry: PropTypes.instanceOf(Map)
+};
 
 export default HomeView;
